5b9c54e8bfd96abd41450fcd84c1a8091025ea14
Ozgur Altug <ozguraltug06@gmail.com>
2023-04-05 15:47:50 +0300

    v1

--- src/Karisik/ThreadExample01.java
+++ src/Karisik/ThreadExample01.java
@@ -2,29 +2,41 @@ package Karisik;
 
 public class ThreadExample01 {
 static int i,j;
-
-
     public static void main(String[] args) {
-        Thread t1=new Thread(){
-            @Override
-            public void run() {
-                for(i=0;i<100;i++)
-                System.out.println("domatesin uzunluğu "+ i+" cm oldu");
+Thread t1=new Thread(){
+    @Override
+    public void run() {
+        for(i=1;i<100;i++){
+            try {
+                System.out.println("Domatesin boyu "+i+" cm oldu, büyüyor");
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-        };
-
-        Thread t2=new Thread(){
-            @Override
-            public void run() {
-                for(j=0;j<100;j++){
-                    System.out.println("salatalık "+j+" cm oldu");
-                }
-            }
-        };
-
-        t1.start();
-        t2.start();
 
         }
+
+    }
+};
+Thread t2=new Thread(){
+    @Override
+    public void run() {
+        for(j=1;j<100;j++){
+            try { System.out.println("Salatalığın boyu "+j+" cm oldu");
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+
+    }
+};
+
+
+  t1.start();
+  t2.start();
+    }
+
     }
 
