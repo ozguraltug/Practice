dd4b2b1b5a75f1b3e535310d7c6d66e5631138ae
Ozgur Altug <ozguraltug06@gmail.com>
2023-04-05 15:34:59 +0300

    2

--- src/Karisik/ThreadExample01.java
+++ src/Karisik/ThreadExample01.java
@@ -1,16 +1,30 @@
 package Karisik;
 
-public class ThreadExample01 extends Thread{
+public class ThreadExample01 {
 static int i,j;
 
-    @Override
-    public void run() {
-        super.run();
-    }
 
     public static void main(String[] args) {
-        Thread t1=new Thread();
+        Thread t1=new Thread(){
+            @Override
+            public void run() {
+                for(i=0;i<100;i++)
+                System.out.println("domatesin uzunluğu "+ i+" cm oldu");
+            }
+        };
+
+        Thread t2=new Thread(){
+            @Override
+            public void run() {
+                for(j=0;j<100;j++){
+                    System.out.println("salatalık "+j+" cm oldu");
+                }
+            }
+        };
+
+        t1.start();
+        t2.start();
 
         }
     }
-}
+
