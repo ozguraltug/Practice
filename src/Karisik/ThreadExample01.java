3d2af8ce1e80a5736d86f733b38a689b1064ea0f
Ozgur Altug <ozguraltug06@gmail.com>
2023-04-05 12:44:09 +0300

    1

---
+++ src/Karisik/ThreadExample01.java
@@ -0,0 +1,16 @@
+package Karisik;
+
+public class ThreadExample01 extends Thread{
+static int i,j;
+
+    @Override
+    public void run() {
+        super.run();
+    }
+
+    public static void main(String[] args) {
+        Thread t1=new Thread();
+
+        }
+    }
+}
--- src/Karisik/thread.java
+++
@@ -1,2 +0,0 @@
-package Karisik;public class thread {
-}
