2249885debde3dfe391d1bc47324b86e35f61da7
Ozgur Altug <ozguraltug06@gmail.com>
2023-04-08 12:54:47 +0300

    4

--- src/Karisik/ThreadExample04.java
+++ src/Karisik/ThreadExample04.java
@@ -4,12 +4,32 @@ public class ThreadExample04 implements Runnable{
 
     public static void main(String[] args) {
 
-        Thread th1=new Thread();
-        Thread th2=new Thread();
+        Thread th1=new Thread(new ThreadExample04("Domates"));
+        Thread th2=new Thread(new ThreadExample04("Salatalık"));
+        th1.start();
+        th2.start();
 
     }
+    String name;
+    public ThreadExample04(String name) {
+        this.name = name;
+    }
+
+
+
     @Override
     public void run() {
+
+            try {for(int i=1;i<=100;i++) {
+
+                System.out.println(this.name + " boyu " + i + " cm oldu");
+                Thread.sleep(500);
+            }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+
 
     }
 }
