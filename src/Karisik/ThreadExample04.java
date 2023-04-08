db3e643e7f6c1b771edad571d468cdfa24ce1946
Ozgur Altug <ozguraltug06@gmail.com>
2023-04-08 12:43:54 +0300

    1

---
+++ src/Karisik/ThreadExample04.java
@@ -0,0 +1,15 @@
+package Karisik;
+
+public class ThreadExample04 implements Runnable{
+
+    public static void main(String[] args) {
+
+        Thread th1=new Thread();
+        Thread th2=new Thread();
+
+    }
+    @Override
+    public void run() {
+
+    }
+}
